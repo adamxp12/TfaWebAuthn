1c8f260a6e5629f41189c6547a0188ef7d8df174
androbey <7.obermeier@gmail.com>
2022-06-06 23:34:50 +0200

    Version bump

--- TfaWebAuthn.info.php
+++ TfaWebAuthn.info.php
@@ -2,7 +2,7 @@
 $info = array(
 	'title' => 'WebAuthn two-factor authentication ',
 	'summary' => 'For modern two factor authentication with U2F keys and on device credentials like Fingerprint',
-	'version' => 100,
+	'version' => 101,
 	'singular' => true,
 	'requires' => 'ProcessWire>=3.0.109'
 );
